53c9caf802461f98f2e25a59b4005807512cc577
Laurens Martina <laurenzz@gmail.com>
2021-09-06 00:24:04 +0200

    Subject view: Adds source downloads

--- resources/views/subjects/detail.blade.php
+++ resources/views/subjects/detail.blade.php
@@ -14,6 +14,25 @@
                 <br>
                 <b>Dit moet onderzocht worden omdat:</b><br>
                 <span>{!! nl2br($subject->importance) !!}</span><br>
+                <br>
+                <b>Bronnen bij dit onderwerp:</b><br>
+                <div class="container">
+                    @foreach($subject->suggestion->files as $file)
+                        <div class="row">
+                            <div class="col-1">
+                                @if($file->isImage())
+                                    <img src="{{ asset($file->file_path) }}" style="width: inherit;">
+                                @else
+                                    <div class="border p-2 border-primary">{{ $file->getExtension() }}</div>
+                                @endif
+                            </div>
+                            <div>
+                                {{ $file->name }}<br>
+                                <a href="{{ asset($file->file_path) }}" download>download</a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </li>
